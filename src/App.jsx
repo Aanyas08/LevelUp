fd0e8eecc147752c2b95cceb7387c07e44b9c2d2
Aditya <adityasoniji4555@gmail.com>
2026-08-20 18:25:03 +0530

    Made minor changes in UI

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@ import ProtectedRoute from './auth/ProtectedRoute.jsx'
 
 function AppShell() {
   return (
-    <div className="flex min-h-screen bg-bg">
+    <div className="flex h-screen bg-bg">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
         <Topbar />
-        <main className="flex-1 px-8 py-6">
+        <main className="flex-1 px-8 py-6 overflow-auto">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/features" element={<Features />} />
